4ca659573e65297b15afd961ccefc6e1ac77db00
JoseV-001 <Leaphaz@gmail.com>
2025-12-04 21:04:11 -0300

    Add initial JDBC demos repository structure

---
+++ jdbc03/src/db/DbException.java
@@ -0,0 +1,12 @@
+package db;
+
+public class DbException extends RuntimeException {
+
+    private static final long serialVersionUID = 1;
+
+    public DbException(String msg){
+        super(msg);
+    }
+
+
+}
